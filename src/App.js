b5d0fa9011a7708e0be5d3d1f96ef465c4626c76
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-07-09 08:19:43 +0530

    added svg folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+// import cart from "./";
 
 function App() {
-  return <div className="App">good morning saurabh</div>;
+  return (
+    <div className="App">
+      good morning saurabh
+      <img src="svg/search.svg" alt="" />
+    </div>
+  );
 }
 
 export default App;
