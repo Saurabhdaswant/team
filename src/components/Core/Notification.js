c3fe1e4dee84b2581bfc2113b142e83690aa1e99
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-13 00:07:47 +0530

    added modal component

--- src/components/Core/Notification.js
+++ src/components/Core/Notification.js
@@ -1,35 +1,42 @@
 import { Switch, Route } from "react-router-dom";
+import { useState } from "react";
 import Fut from "../Fut";
+import Modal from "../Modal";
 
 const Notification = () => {
+
+  const [modal] = useState(true)
+  
   return (
     <div className="notification">
       <div className="main-child-two">
-        <div className="default">
-          <div className="theHead">
-            <div className="head">
-              <img src="svg/Bell.svg" alt="" />
-              <p>Notifications</p>
-            </div>
-            <h3 className="day">Today</h3>
-          </div>
-          <div className="theHead__notification">
-            <Fut />
-            <Fut />
-            <Fut />
-            <Fut />
-            <Fut />
-            <Fut />
-            <Fut />
-            <Fut />
-            <Fut />
-            <Fut />
-            <Switch>
-              <Route exact path="/" component={Fut} />
-              <Route path="/Fut" component={Fut} />
-            </Switch>
-          </div>
-        </div>
+{
+  modal ? <Modal/> : <div className="default">
+  <div className="theHead">
+    <div className="head">
+      <img src="svg/Bell.svg" alt="" />
+      <p>Notifications</p>
+    </div>
+    <h3 className="day">Today</h3>
+  </div>
+  <div className="theHead__notification">
+    <Fut />
+    <Fut />
+    <Fut />
+    <Fut />
+    <Fut />
+    <Fut />
+    <Fut />
+    <Fut />
+    <Fut />
+    <Fut />
+    <Switch>
+      <Route exact path="/" component={Fut} />
+      <Route path="/Fut" component={Fut} />
+    </Switch>
+  </div>
+</div>
+}
       </div>
     </div>
   );
