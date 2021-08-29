8ab9ae9c6cf693a80e963354450b1b95c55a9aaf
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-29 18:34:03 +0530

    add NewOrder.js

---
+++ src/components/NewOrder.js
@@ -0,0 +1,24 @@
+const NewOrder = () => {
+  return (
+    <div className="notifications">
+      <div className="">
+        <div className="notification__order">
+          <p className="notification__type">New Order</p>
+          <p className="notification__id">#238478</p>
+        </div>
+        <div className="notification--ordertype">
+          <div className="orderImg">
+            <img src="svg/shirt.png" alt="" />
+          </div>
+          <div className="notification__orderName">
+            <p className="order__name">Bella Canvas</p>
+            <p>orange</p>
+          </div>
+        </div>
+        <div className="order__time">9:53 am</div>
+      </div>
+    </div>
+  );
+};
+
+export default NewOrder;
