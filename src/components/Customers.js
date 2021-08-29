5a87a0436148569623cb1954dbfca8df94d82a23
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-30 00:05:37 +0530

    i made changes but i am not able to see it in prod

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,5 +1,20 @@
+import CustomerDetail from "./CustomerDetail"
 const Customers = () => {
-  return <h1>get Customers</h1>;
+  return <div className="CustomerService" >
+  <div className="CustomerService__CustomerDetail">
+    <div className="CustomerService__CustomerDetail--head">
+      <p className="bold">Name</p>
+      <p className="bold">Adress</p>
+      <p className="bold">Phone no.</p>
+      <p className="bold">Email ID</p>
+    </div>
+  </div>
+  <CustomerDetail/>
+  <CustomerDetail/>
+  <CustomerDetail/>
+  <CustomerDetail/>
+  <CustomerDetail/>
+</div>
 };
 
 export default Customers;
