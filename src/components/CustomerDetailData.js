effbd9719b9852ab6ad8d27536dcfc5b2a924d7e
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-29 18:54:31 +0530

    add temporary data of customers detail info

---
+++ src/components/CustomerDetailData.js
@@ -0,0 +1,48 @@
+const customerDetail = [
+    {
+      id: 1,
+      Name: "Robert",
+      Address: "928 S College Ave Louisville, Georgia(GA), 30441",
+      PhoneNo: 4785543367,
+      EmailID: "shdbfihzsb@gmail.com",
+    },
+    {
+        id: 2,
+        Name: "Jaime",
+        Address: "928 S College Ave Louisville, Georgia(GA), 30441",
+        PhoneNo: 4785543367,
+        EmailID: "shdbfihzsb@gmail.com",
+      },    {
+        id: 3,
+        Name: "Catelyn ",
+        Address: "928 S College Ave Louisville, Georgia(GA), 30441",
+        PhoneNo: 4785543367,
+        EmailID: "shdbfihzsb@gmail.com",
+      },    {
+        id: 4,
+        Name: "Cersei ",
+        Address: "928 S College Ave Louisville, Georgia(GA), 30441",
+        PhoneNo: 4785543367,
+        EmailID: "shdbfihzsb@gmail.com",
+      },    {
+        id: 5,
+        Name: "Daenerys ",
+        Address: "928 S College Ave Louisville, Georgia(GA), 30441",
+        PhoneNo: 4785543367,
+        EmailID: "shdbfihzsb@gmail.com",
+      },    {
+        id: 6,
+        Name: "Jorah ",
+        Address: "928 S College Ave Louisville, Georgia(GA), 30441",
+        PhoneNo: 4785543367,
+        EmailID: "shdbfihzsb@gmail.com",
+      },    {
+        id: 7,
+        Name: "Viserys ",
+        Address: "928 S College Ave Louisville, Georgia(GA), 30441",
+        PhoneNo: 4785543367,
+        EmailID: "shdbfihzsb@gmail.com",
+      },
+  ];
+  export default customerDetail;
+  
