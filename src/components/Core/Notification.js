b84e6076e518f69a225065452e9ea827fdbe7fb6
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-29 18:35:26 +0530

    add notification_components

--- src/components/Core/Notification.js
+++ src/components/Core/Notification.js
@@ -1,13 +1,13 @@
 import { Switch, Route } from "react-router-dom";
 import MainMsg from "../MainMsg";
 import Modal from "../Modal";
-import OrdersShopNotification from "../OrdersShopNotification"
+import notification_components from "../notification_components"
 const Notification = () => {
   return (
     <div className="notification">
       <div className="main-child-two">
         <Switch>
-        <Route exact path="/" component={OrdersShopNotification} />
+        <Route exact path="/" component={notification_components} />
         <Route path="/CustomerService" component={MainMsg} />
         <Route path="/Customers" component={Modal} />
         </Switch>
