5223cbce5c13ed5976ef19a379446f8f9f7d687c
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-29 18:41:02 +0530

    build MainMsg
     the component that shows the particular feedback msg from client

---
+++ src/components/MainMsg.js
@@ -0,0 +1,23 @@
+const MainMsg = () => {
+  return (
+    <div className="MainMsg">
+      <div className="modalComponent__head--button">
+        <button>close X</button>
+      </div>
+      <p className="bold ">Message</p>
+      <p>
+        I was looking for the quality garments and finally got it. It’s great
+        buying from inkredible and I love their services, Online and Offline. I
+        always wanted to customize my own design but keep the quality and I was
+        looking for the quality garments and finally got it. It’s great buying
+        from inkredible and I love their services, Online and Offline. I always
+        wanted to customize my own design but keep the quality and I was looking
+        for the quality garments and finally got it. It’s great buying from
+        inkredible and I love their services, Online and Offline. I always
+        {/* wanted to customize my own design but keep the quality and{" "} */}
+      </p>
+    </div>
+  );
+};
+
+export default MainMsg;
