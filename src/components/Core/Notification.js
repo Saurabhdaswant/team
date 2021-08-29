095c5b81aaf14be2c5945246288259f8018e02d0
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-29 18:21:55 +0530

    cleaned the whole thing and putted everthing in components

--- src/components/Core/Notification.js
+++ src/components/Core/Notification.js
@@ -1,42 +1,16 @@
 import { Switch, Route } from "react-router-dom";
-import { useState } from "react";
-import Message from "../Message"
+import MainMsg from "../MainMsg";
 import Modal from "../Modal";
-
+import OrdersShopNotification from "../OrdersShopNotification"
 const Notification = () => {
-
-  const [modal] = useState(true)
-  
   return (
     <div className="notification">
       <div className="main-child-two">
-{
-  modal ? <Modal/> : <div className="default">
-  <div className="theHead">
-    <div className="head">
-      <img src="svg/Bell.svg" alt="" />
-      <p>Notifications</p>
-    </div>
-    <h3 className="day">Today</h3>
-  </div>
-  <div className="theHead__notification">
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Switch>
-      <Route exact path="/" component={Message} />
-      <Route path="/Message" component={Message} />
-    </Switch>
-  </div>
-</div>
-}
+        <Switch>
+        <Route exact path="/" component={OrdersShopNotification} />
+        <Route path="/CustomerService" component={MainMsg} />
+        <Route path="/Customers" component={Modal} />
+        </Switch>
       </div>
     </div>
   );
