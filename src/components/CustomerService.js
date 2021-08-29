9e0eaf1aeacc16774bddd96f1726abbd699adc41
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-29 18:53:04 +0530

    build CustomerService section
    which shows the Customer Details and there feedback msg

--- src/components/CustomerService.js
+++ src/components/CustomerService.js
@@ -1,5 +1,29 @@
+import CustomerDetail from "./CustomerDetail";
+import { useState } from "react";
+import customerDetail from "./CustomerDetailData";
+
+
 const CustomerService = () => {
-  return <h1>Customer Service</h1>;
+  const [toogle, settoogle] = useState(0);
+
+  return (
+    <div className="CustomerService">
+      <div className="CustomerService__CustomerDetail">
+        <div className="CustomerService__CustomerDetail--head">
+          <p className="bold">Name</p>
+          <p className="bold">Adress</p>
+          <p className="bold">Phone no.</p>
+          <p className="bold">Email ID</p>
+        </div>
+      </div>
+      <div className="customerDetail">
+        {customerDetail.map((detail) => {
+          const { id } = detail;
+          return <CustomerDetail key={id} detail={detail} toogle={toogle}  settoogle={settoogle} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default CustomerService;
