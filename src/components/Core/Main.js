8d1dbc81d9403d81bfb5bbb62520ba48e964c7d2
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-29 18:24:15 +0530

    some cleaning

--- src/components/Core/Main.js
+++ src/components/Core/Main.js
@@ -23,49 +23,16 @@ const Main = () => {
             <p>completed</p>
           </div>
         </div>
-        <div className="main__childOne__navbar">
-          <nav className="navbar__nav">
-            <div className="nav__item nav__item--active">short</div>
-            <div className="nav__item">long</div>
-            <div className="nav__item">hoodies</div>
-            <div className="nav__item">crewnecks</div>
-            <div className="nav__item">canvas</div>
-            <div className="nav__item">stickers</div>
-          </nav>
-        </div>
-
-        <div className="main__childOne__orders">
-          <div className="orders--type">
-            <h2>Pending orders</h2>
-          </div>
-          <div className="orders__filterAndsort">
-            <div className="filter">filter</div>
-            <div className="sort">sort by</div>
-          </div>
-        </div>
-
-        <div className="main__childOne__progressChart">
-          <div className="progreesContainer">
-            <div className="title">
-              <p className="product">product</p>
-              <p>order id</p>
-              <p>category</p>
-              <p>size</p>
-              <p>Quanity</p>
-              <p>Date</p>
-              <p>Price</p>
-            </div>
-            <Switch>
+        
+        <Switch>
               <Route exact path="/" component={OrdersShop} />
               <Route  path="/OrdersQuote" component={OrdersQuote} />
               <Route path="/Fundraise" component={Fundraise} />
               <Route path="/CustomerService" component={CustomerService} />
-              <Route path="/Customer" component={Customer} />
+              <Route path="/Customers" component={Customer} />
               <Route path="/Statictis" component={Statictis} />
               <Route path="/Money" component={Money} />
             </Switch>
-          </div>
-        </div>
       </div>
     </div>
   );
