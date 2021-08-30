7c232cdf562b056c45c43fd423cf42204ed48c0d
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-30 16:53:36 +0530

    Notfication Component folder name changes

--- src/components/Core/Notification.js
+++ src/components/Core/Notification.js
@@ -1,7 +1,8 @@
 import { Switch, Route } from "react-router-dom";
 import MainMsg from "../MainMsg";
 import Modal from "../Modal";
-import Notification_components from "../Notification_components"
+import Notification_components from "../Notification_components";
+
 const Notification = () => {
   return (
     <div className="notification">
