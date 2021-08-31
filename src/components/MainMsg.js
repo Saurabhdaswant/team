21d7003c826bb0e3b601b24d330df58662a39a55
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-31 17:58:25 +0530

    add download btn and improved the responsiveness of app

--- src/components/MainMsg.js
+++ src/components/MainMsg.js
@@ -1,9 +1,10 @@
 const MainMsg = () => {
   return (
-    <div className="MainMsg">
+    <div className="MainMsg_Container">
       <div className="modalComponent__head--button">
-        <button>close X</button>
+        <button>close x</button>
       </div>
+      <div className="MainMsg">
       <p className="bold ">Message</p>
       <p>
         I was looking for the quality garments and finally got it. It’s great
@@ -14,8 +15,9 @@ const MainMsg = () => {
         wanted to customize my own design but keep the quality and I was looking
         for the quality garments and finally got it. It’s great buying from
         inkredible and I love their services, Online and Offline. I always
-        {/* wanted to customize my own design but keep the quality and{" "} */}
+        wanted to customize my own design but keep the quality and{" "}
       </p>
+      </div>
     </div>
   );
 };
