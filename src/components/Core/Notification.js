fcc36f6e9f70244fff66fde685c4aeea8f6dd20e
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-08-31 19:42:40 +0530

    fix file name

--- src/components/Core/Notification.js
+++ src/components/Core/Notification.js
@@ -1,14 +1,14 @@
 import { Switch, Route } from "react-router-dom";
 import MainMsg from "../MainMsg";
 import Modal from "../Modal";
-import Notification_components from "../Notification_components";
+import notification_components from "../notification_components";
 
 const Notification = () => {
   return (
     <div className="notification">
       <div className="main-child-two">
         <Switch>
-        <Route exact path="/" component={Notification_components} />
+        <Route exact path="/" component={notification_components} />
         <Route path="/CustomerService" component={MainMsg} />
         <Route path="/Customers" component={Modal} />
         </Switch>
