01e05d59f863c477937885dcb862b2ba2aa0f2c8
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-09-06 16:36:34 +0530

    delete customers route from the file

--- src/components/Core/Notification.js
+++ src/components/Core/Notification.js
@@ -1,16 +1,35 @@
 import { Switch, Route } from "react-router-dom";
 import MainMsg from "../MainMsg";
 import Modal from "../Modal";
-import NotificationComponents from "../NotificationComponents"
+import NotificationComponents from "../NotificationComponents";
 
-const Notification = ({showModal , setShowModal , showMessage , setShowMessage}) => {
+const Notification = ({
+  showModal,
+  setShowModal,
+  showMessage,
+  setShowMessage,
+}) => {
   return (
     <div className="notification">
       <div className="main-child-two">
         <Switch>
-        <Route exact path="/"> <NotificationComponents showModal={showModal} setShowModal={setShowModal} Modal={Modal} /></Route>
-        <Route  path="/CustomerService"> <MainMsg showMessage={showMessage} setShowMessage={setShowMessage} NotificationComponents={NotificationComponents}  /></Route>
-        <Route path="/Customers" component={Modal} />
+          <Route exact path="/">
+            {" "}
+            <NotificationComponents
+              showModal={showModal}
+              setShowModal={setShowModal}
+              Modal={Modal}
+            />
+          </Route>
+          <Route path="/CustomerService">
+            {" "}
+            <MainMsg
+              showMessage={showMessage}
+              setShowMessage={setShowMessage}
+              NotificationComponents={NotificationComponents}
+            />
+          </Route>
+          {/* <Route path="/Customers" component={Modal} /> */}
         </Switch>
       </div>
     </div>
